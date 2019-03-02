3c9733052f34651435d2adfce6e50d91af444680
Gareth van der Berg <gareth.shaun@gmail.com>
2019-03-02 12:38:01 +0200

    update script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-curl -sSL get.docker.com | sh && usermod pi -aG docker
+curl -sSL get.docker.com | sh && \
+sudo usermod pi -aG docker
 
-dphys-swapfile swapoff && \
-    dphys-swapfile uninstall #&& \
-    #update-rc.d dphys-swapfile remove
+sudo dphys-swapfile swapoff && \
+sudo dphys-swapfile uninstall && \
+sudo update-rc.d dphys-swapfile remove
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
+sudo apt-get update -q && \
+sudo apt-get install -qy kubeadm
