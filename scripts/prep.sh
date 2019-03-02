36484fa79db91e02102884082844c25d8d2eaa0e
Gareth van der Berg <gareth.shaun@gmail.com>
2019-03-02 13:34:18 +0200

    update script

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-echo 1) Install Docker
+echo "1) Install Docker"
 
 curl -sSL get.docker.com | sh && \
 sudo usermod pi -aG docker
 
-echo 2) Disable swap
+echo "2) Disable swap"
 
 sudo dphys-swapfile swapoff && \
 sudo dphys-swapfile uninstall && \
 sudo update-rc.d dphys-swapfile remove
 
-echo 3) Add repo lists & install kubeadm
+echo "3) Add repo lists & install kubeadm"
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
 sudo apt-get update -q && \
 sudo apt-get install -qy kubeadm
 
-echo 4) Adding " cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" to /boot/cmdline.txt
+echo "4) Adding \" cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory\" to /boot/cmdline.txt"
 
 sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
 orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
 echo $orig | sudo tee /boot/cmdline.txt
 
-echo 5) Please reboot
+echo "5) Please reboot"
