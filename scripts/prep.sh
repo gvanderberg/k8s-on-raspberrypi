e2e4e3b6467a9b8a11217acc2c1bfb379d9ee61f
Gareth van der Berg <gareth.shaun@gmail.com>
2019-03-02 12:46:22 +0200

    update script

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -18,3 +18,4 @@ sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
 orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
 echo $orig | sudo tee /boot/cmdline.txt
 
+echo Please reboot
