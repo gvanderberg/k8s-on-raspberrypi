47b319208571fe7ac020f94a6b5537a94b0d4cb3
Gareth van der Berg <gareth.shaun@gmail.com>
2019-11-03 11:38:49 +0200

    Update prep.sh

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -7,21 +7,21 @@ usermod pi -aG docker
 
 echo 2\) Disable swap
 
-sudo dphys-swapfile swapoff && \
-sudo dphys-swapfile uninstall && \
-sudo update-rc.d dphys-swapfile remove
+dphys-swapfile swapoff && \
+dphys-swapfile uninstall && \
+update-rc.d dphys-swapfile remove
 
 echo 3\) Add repo lists \& install kubeadm
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
-sudo apt-get update -q && \
-sudo apt-get install -qy kubeadm
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list && \
+apt-get update -q && \
+apt-get install -qy kubeadm
 
 echo 4\) Adding " cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" to /boot/cmdline.txt
 
-sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
+cp /boot/cmdline.txt /boot/cmdline_backup.txt
 orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
-echo $orig | sudo tee /boot/cmdline.txt
+echo $orig | tee /boot/cmdline.txt
 
 echo 5\) Please reboot
