46b5e4f4eaa6913834dd91a80e13caf51dace102
Gareth van der Berg <gareth.shaun@gmail.com>
2020-10-01 15:41:08 +0200

    Update prep.sh

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -3,20 +3,20 @@
 echo 1\) Install Docker
 
 curl -fSLs https://get.docker.com | sh && \
-usermod pi -aG docker
+  usermod pi -aG docker
 
 echo 2\) Disable swap
 
 dphys-swapfile swapoff && \
-dphys-swapfile uninstall && \
-update-rc.d dphys-swapfile remove
+  dphys-swapfile uninstall && \
+  update-rc.d dphys-swapfile remove
 
 echo 3\) Add repo lists \& install kubeadm
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list && \
 apt-get update -q && \
-apt-get install -qy kubeadm
+  apt-get install -qy kubeadm
 
 echo 4\) Adding " cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" to /boot/cmdline.txt
 
